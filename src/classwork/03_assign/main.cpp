10d2cd811177d0ce658885d30c9f23677b28cc07
Hannah Kim <chehyun.kim@g.austincc.edu>
2021-03-21 05:37:23 +0000

    Homework3

--- src/classwork/03_assign/main.cpp
+++ src/classwork/03_assign/main.cpp
@@ -1,10 +1,31 @@
 //Write the include statement for decisions.h here
+#include "decision.h"
+#include <iostream>
 
 //Write namespace using statements for cout and cin
+using std::cout;
+using std::cin;
+using std::endl;
 
 int main() 
 {
+  int grade;
+  string result_if, result_switch;
 
+  cout<<"Enter your grade: ";
+  cin >> grade;
+
+  if (grade >= 0 && grade <=100)
+  {
+    result_if = get_letter_grade_using_if(grade);
+    result_switch = get_letter_grade_using_switch(grade);
+    cout << "Your letter grade is " << result_if << endl;
+    cout << "Your letter grade is " << result_switch<<endl;
+  }
+  else 
+  {
+    cout << "number out of range."<<endl;;
+  }
 	return 0;
 }
 
