d4eb2c2ffbe57044541191337c672272b2700dd4
Hannah Kim <chehyun.kim@g.austincc.edu>
2021-03-21 06:18:54 +0000

    Homework4

--- src/classwork/04_assign/loops.cpp
+++ src/classwork/04_assign/loops.cpp
@@ -1,1 +1,12 @@
 //cpp
+#include "loops.h"
+
+int factorial(int num)
+{
+  int fac = 1, i;
+  for (i=1;i<=num;i++)
+  {
+    fac = fac * i;
+  }
+  return fac;
+}
--- src/classwork/04_assign/main.cpp
+++ src/classwork/04_assign/main.cpp
@@ -1,7 +1,11 @@
 //write includes statements
+#include "loops.h"
+#include <iostream>
 
 //write using statements for cin and cout
-
+using std::cin;
+using std::cout;
+using std::endl;
 
 /*
 Use a do while loop to prompt the user for 
@@ -10,6 +14,21 @@ factorial.  Also, loop continues as long as user wants to.
 */
 int main() 
 {
+  char choice;
+  do
+  {
+    int num;
+    do
+    {
+      cout<<"Enter a number between 1 to 10: "<<endl;
+      cin >> num;
+    }
+    while (num>10||num<1);
+
+    cout << factorial(num)<<endl<<"Enter 'Y' to continue: ";
+    cin>>choice;
+  }
+  while (choice == 'Y');
 
 	return 0;
 }
