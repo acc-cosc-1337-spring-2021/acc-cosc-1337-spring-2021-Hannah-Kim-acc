28786848b0b494dcbfdb3fde19d40b28f7ab55de
Hannah Kim <chehyun.kim@g.austincc.edu>
2021-03-28 03:15:02 +0000

    Homework 5

--- src/homework/04_iteration/dna.cpp
+++ src/homework/04_iteration/dna.cpp
@@ -1,4 +1,9 @@
 #include "dna.h"
+#include <iostream>
+
+
+
+
 /*
 Write code for function get_gc_content that accepts
 a const reference string parameter and returns a double.
@@ -9,12 +14,43 @@ Return quotient.
 
 
 
+double get_gc_content(const string& dna)
+{
+  double gc_count=0, total_count = 0;
+  double gc_content=0;
+
+  total_count = dna.size();
+
+  for(int i=0;i<total_count;i++)
+  {
+    if (dna[i]=='G'||dna[i]=='C')
+    {
+      gc_count+=1;
+    }
+  }
+  gc_content = gc_count/total_count;
+
+  return gc_content;
+}
+
 
 /*
 Write code for function get_reverse_string that
 accepts a string parameter and returns a string reversed.
 */
+string  reverse_string(string dna)
+{
+  int i,len = dna.size()-1;
+  string reverse_dna(dna.size(),' ') ;
 
+  for (i=len;i>=0;i--)
+  {
+    reverse_dna[i]=dna[len-i];
+    //dna[i]=dna[dna.size()-i-1];
+    
+  }
+  return reverse_dna;
+}
 
 
 /*
@@ -28,3 +64,36 @@ c. return string
 
 */
 
+string get_dna_complement(string dna)
+{
+  string reverse_dna = " ";
+  reverse_dna = reverse_string(dna);
+
+  int i = 0;
+  while(i<reverse_dna.size())
+  {
+    if(reverse_dna[i]=='G')
+    {
+      reverse_dna[i]='C';
+      i+=1;
+    }
+    else if(reverse_dna[i]=='C')
+    {
+      reverse_dna[i]='G';
+      i+=1;
+    }
+    else if(reverse_dna[i]=='T')
+    {
+      reverse_dna[i]='A';
+      i+=1;
+    }
+    else if(reverse_dna[i]=='A')
+    {
+      reverse_dna[i]='T';
+      i+=1;
+    }
+  }
+
+  return reverse_dna;
+
+}
--- src/homework/04_iteration/main.cpp
+++ src/homework/04_iteration/main.cpp
@@ -1,7 +1,9 @@
 //write include statements
-
+#include "dna.h"
+#include <iostream>
 //write using statements
-
+using std::cin;
+using std::cout;
 
 /*
 Write code that prompts user to enter 1 for Get GC Content, 
@@ -10,7 +12,58 @@ DNA string and call either get gc content or get dna complement
 function and display the result. Program runs as long as 
 user enters a y or Y.
 */
+
 int main() 
 {
+  string input;
+  /*
+  do
+  {
+    int num = 0;
+    string dna;
+    cout<<"Enter 1 for Get GC Content, 2 for DNA Complement: ";
+    cin>>num;
+    switch(num)
+    {
+      case 1: 
+      cout<<"Enter a DNA string to get GC content: ";
+      cin>>dna;
+      cout<<"The GC content is: "<<get_gc_content(dna)<<"\n";
+      break;
+      case 2:
+      cout<<"Enter a DNA string to get DNA complement: ";
+      cin>>dna;
+      cout<<"The DNA complement is: "<<get_dna_complement(dna)<<"\n";
+      break;
+    }
+    cout<<"Enter 'Y' or 'y' to continue: ";
+    cin>>input;
+  }
+  while(input=='Y'||input == 'y');
+  */
+
+  while(input != "Y"&&input != "y")
+  {
+    int num = 0;
+    string dna;
+    cout<<"Enter 1 for Get GC Content, 2 for DNA Complement: ";
+    cin>>num;
+    switch(num)
+    {
+      case 1: 
+      cout<<"Enter a DNA string to get GC content: ";
+      cin>>dna;
+      cout<<"The GC content is: "<<get_gc_content(dna)<<"\n";
+      break;
+      case 2:
+      cout<<"Enter a DNA string to get DNA complement: ";
+      cin>>dna;
+      cout<<"The DNA complement is: "<<get_dna_complement(dna)<<"\n";
+      break;
+    }
+    cout<<"Enter 'Y' or 'y' to exit: ";
+    cin>>input;
+  }
+
 	return 0;
 }
